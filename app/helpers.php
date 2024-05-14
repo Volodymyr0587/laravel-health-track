15ab24f990eb1f9a6427021fb77531a9fb2d6b1d
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-05-14 17:37:03 +0300

    sidebar item component; count auth user attachments (files) helper function

--- app/helpers.php
+++ app/helpers.php
@@ -1,3 +1,21 @@
 <?php
 
-//
+if ( !function_exists('count_attachments') ) {
+    function count_attachments() {
+        $userAttachmentsCount = 0;
+        $user = auth()->user();
+
+        if ($user) {
+            $events = $user->events()->get();
+
+            foreach ($events as $event) {
+                $mediaCount = $event->getMedia('attachments')->count();
+                if ($mediaCount > 0) {
+                    $userAttachmentsCount += $mediaCount;
+                }
+            }
+        }
+
+        return $userAttachmentsCount;
+    }
+}
--- resources/views/components/sidebar/sidebar.blade.php
+++ resources/views/components/sidebar/sidebar.blade.php
@@ -1,81 +1,73 @@
 <x-sidebar.layout>
-            @auth
-            <li>
-                <div class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white group">
-                    <x-svg.user />
-                    <span class="ms-3 font-bold">{{ auth()->user()->first_name }} {{ auth()->user()->last_name }}</span>
-                </div>
-            </li>
-            @endauth
-            <li>
-                <a href="#"
-                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    <x-svg.dashboard />
-                    <span class="flex-1 ms-3 whitespace-nowrap">{{ __("Dashboard") }}</span>
-                </a>
-            </li>
-            <li>
-                <a href="{{ route('events.index') }}"
-                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    <x-svg.health-events />
-                    <span class="flex-1 ms-3 whitespace-nowrap">{{ __('Health events') }}</span>
-                    <span
-                        class="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300">{{
-                        auth()->user()?->events()->count() ?? '' }}</span>
-                </a>
-            </li>
+    @auth
+    <li>
+        <a href="#"
+            class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+            <x-svg.user />
+            <span class="flex-1 ms-3 whitespace-nowrap font-bold">{{ auth()->user()->first_name }} {{ auth()->user()->last_name }}</span>
+        </a>
+    </li>
+    @endauth
+    <li>
+        <a href="#"
+            class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+            <x-svg.dashboard />
+            <span class="flex-1 ms-3 whitespace-nowrap">{{ __("Dashboard") }}</span>
+        </a>
+    </li>
 
-            <li>
-                <a href="{{ route('attachments.index') }}"
-                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    <x-svg.attachments />
-                    <span class="flex-1 ms-3 whitespace-nowrap">{{ __("Attachments") }}</span>
-                    <span
-                        class="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300"></span>
-                </a>
-            </li>
 
-            <li>
-                <a href="#"
-                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    <x-svg.users />
-                    <span class="flex-1 ms-3 whitespace-nowrap">{{ __("Users") }}</span>
-                </a>
-            </li>
-            <li>
-                <a href="#"
-                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    <x-svg.products />
-                    <span class="flex-1 ms-3 whitespace-nowrap">{{ __("Products") }}</span>
-                </a>
-            </li>
-            @guest
-            <li>
-                <a href="{{ route('login') }}"
-                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    <x-svg.sign-in />
-                    <span class="flex-1 ms-3 whitespace-nowrap">{{ __("Sign In") }}</span>
-                </a>
-            </li>
-            <li>
-                <a href="{{ route('register') }}"
-                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    <x-svg.sign-up />
-                    <span class="flex-1 ms-3 whitespace-nowrap">{{ __("Sign Up") }}</span>
-                </a>
-            </li>
-            @endguest
+    <x-sidebar.item routeName="events.index" label="{{ __('Health events') }}" >
+        <x-svg.health-events />
+    </x-sidebar.item>
 
-            @auth
-            <li
-                class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                <x-svg.logout />
-                <form action="{{ route('logout') }}" method="POST">
-                    @csrf
+    <x-sidebar.item routeName="attachments.index" label="{{ __('Attachments') }}" badge="{{ count_attachments() }}">
+        <x-svg.attachments />
+    </x-sidebar.item>
 
-                    <button type="submit" class="flex-1 ms-3 whitespace-nowrap">{{ __("Log Out") }}</button>
-                </form>
-            </li>
-            @endauth
+
+
+    <li>
+        <a href="#"
+            class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+            <x-svg.users />
+            <span class="flex-1 ms-3 whitespace-nowrap">{{ __("Users") }}</span>
+        </a>
+    </li>
+    <li>
+        <a href="#"
+            class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+            <x-svg.products />
+            <span class="flex-1 ms-3 whitespace-nowrap">{{ __("Products") }}</span>
+        </a>
+    </li>
+    @guest
+    <li>
+        <a href="{{ route('login') }}"
+            class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+            <x-svg.sign-in />
+            <span class="flex-1 ms-3 whitespace-nowrap">{{ __("Sign In") }}</span>
+        </a>
+    </li>
+    <li>
+        <a href="{{ route('register') }}"
+            class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+            <x-svg.sign-up />
+            <span class="flex-1 ms-3 whitespace-nowrap">{{ __("Sign Up") }}</span>
+        </a>
+    </li>
+    @endguest
+
+    @auth
+    <li
+        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+        <x-svg.logout />
+        <form action="{{ route('logout') }}" method="POST">
+            @csrf
+
+            <button type="submit" class="flex-1 ms-3 whitespace-nowrap">{{ __("Log Out") }}</button>
+        </form>
+    </li>
+    @endauth
 
 </x-sidebar.layout>
