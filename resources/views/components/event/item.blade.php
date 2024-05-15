cc1df0d5f6dbad5b02e1f84e13d0705bd77cc6c6
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-05-15 15:40:34 +0300

    event item component

---
+++ resources/views/components/event/item.blade.php
@@ -0,0 +1,20 @@
+@props(['eventRoute', 'eventName', 'eventLocation', 'eventTime', 'eventPrice', 'eventUpdatedAt'])
+
+<li class="flex justify-between gap-x-6 py-5">
+    <div class="flex min-w-0 gap-x-4">
+        <x-svg.heart />
+        <div class="min-w-0 flex-auto">
+            <p class="text-base truncate font-semibold leading-6 text-healthtrack-light dark:text-healthtrack-dark">{{ $eventName }}</p>
+            <a href="{{ $eventRoute }}"
+                class="mt-1 truncate text-sm leading-5 text-blue-600 transition-all duration-150 ease-in-out hover:underline hover:font-bold ">
+                {{ __("Show details...") }}
+            </a>
+            <p class="mt-1 truncate text-xs leading-5 text-gray-500">{{ $eventLocation }}</p>
+            <p class="mt-1 truncate text-xs leading-5 text-gray-500">{{ __("Date and time") }}: {{ $eventTime }}</p>
+        </div>
+    </div>
+    <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+        <p class="text-sm leading-6 text-gray-900">{{ __("Price") }}: {{ $eventPrice }}</p>
+        <p class="mt-1 text-xs leading-5 text-gray-500">{{ __("Cerated") }} {{ $eventUpdatedAt }}</p>
+    </div>
+</li>
--- resources/views/events/search.blade.php
+++ resources/views/events/search.blade.php
@@ -10,26 +10,16 @@
                 <x-forms.button url="{{ route('events.index') }}" like="link">Back to Events</x-forms.button>
             </div>
 
-            <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4">
+            <ul role="list" class="divide-y divide-gray-100">
                 @foreach ($events as $event)
 
-                <div
-                    class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        {{ $event->name}}
-                    </h5>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $event->location }}</p>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{
-                        \Carbon\Carbon::parse($event->event_time)->format('d-m-Y H:i:s') }}</p>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $event->price == 0 ? 'Free' :
-                        $event->price }}</p>
-                    <x-forms.button url="{{ route('events.show', $event) }}" like="link">
-                        {{ __('Event Details') }}
-                    </x-forms.button>
-                </div>
+                <x-event.item eventRoute="{{ route('events.show', $event) }}" eventName="{{ $event->name }}"
+                    eventLocation="{{ $event->location }}"
+                    eventTime="{{ \Carbon\Carbon::parse($event->event_time)->format('d-m-Y H:i:s') }}"
+                    eventPrice="{{ $event->price }}" eventUpdatedAt="{{ $event->updated_at->diffForHumans() }}" />
 
                 @endforeach
-            </div>
+            </ul>
         </div>
     </div>
 
