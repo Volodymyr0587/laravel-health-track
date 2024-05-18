9696d14463c16dad84a3284b1f1b2595b5ecae43
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-05-18 21:59:57 +0300

    active sidebar menu item style

--- resources/views/components/sidebar/item.blade.php
+++ resources/views/components/sidebar/item.blade.php
@@ -2,7 +2,7 @@
 
 <li>
     <a href="{{ route($routeName) }}"
-       class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+       class="{{ request()->routeIs($routeName) ? 'font-extrabold bg-gray-700 dark:bg-gray-700' : '' }} flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
         {{ $slot }}
         <span {{ $attributes->merge(['class' => 'flex-1 ms-3 whitespace-nowrap']) }}>{{ $label }}</span>
         @if($badge)
