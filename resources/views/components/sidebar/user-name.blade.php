94fcdfbdce762f446563233c9aa221f71616fbd6
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-05-20 21:54:27 +0300

    Add space x in user name component

--- resources/views/components/sidebar/user-name.blade.php
+++ resources/views/components/sidebar/user-name.blade.php
@@ -1,6 +1,6 @@
 @props(['firstName', 'lastName'])
 
-<li class="flex items-center">
+<li class="flex items-center space-x-3">
     <span {{ $attributes->merge(['class' => 'ms-2 whitespace-nowrap']) }}>{{ $slot }}</span>
     <div class="flex flex-col font-bold text-lg items-start p-0 ml-2 text-gray-900 rounded-lg dark:text-white">
         <span class="break-all">{{ $firstName }}</span>
