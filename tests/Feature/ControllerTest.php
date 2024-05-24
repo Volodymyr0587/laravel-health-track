9e74e7f2dbe139893c987442a92894157bba5340
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-05-24 20:59:23 +0300

    test can delete event

--- tests/Feature/ControllerTest.php
+++ tests/Feature/ControllerTest.php
@@ -1,25 +1,27 @@
 <?php
 
-// use App\Models\User;
-// use App\Models\Event;
+use App\Models\User;
+use App\Models\Event;
 
 // it('can create an event', function () {
 //     $user = User::factory()->create();
-//     // $this->actingAs($user);
+//     $this->actingAs($user);
 
-//     $response = $this->actingAs($user)->post('/events', [
+//     $response = $this->post('/events', [
+//         'user_id' => $user->id,
 //         'name' => 'Doctor Visit',
-//         'event_time' => now(),
+//         'event_time' => now()->toDateTimeString(),
 //         'description' => 'Check-up',
 //         'price' => '100.00',
 //     ]);
 
-//     $response->assertRedirect('/events');
+//     $response->assertRedirect('/');
 //     $this->assertDatabaseHas('events', [
 //         'name' => 'Doctor Visit',
 //         'user_id' => $user->id,
 //     ]);
 // });
+
 
 // it('can update an event', function () {
 //     $user = User::factory()->create();
@@ -43,14 +45,14 @@
 //     ]);
 // });
 
-// it('can delete an event', function () {
-//     $user = User::factory()->create();
-//     $event = Event::factory()->for($user)->create();
+it('can delete an event', function () {
+    $user = User::factory()->create();
+    $event = Event::factory()->for($user)->create();
 
-//     $this->actingAs($user);
+    $this->actingAs($user);
 
-//     $response = $this->delete("/events/{$event->id}");
+    $response = $this->delete("/events/{$event->id}");
 
-//     $response->assertRedirect('/events');
-//     $this->assertDatabaseMissing('events', ['id' => $event->id]);
-// });
+    $response->assertRedirect('/events');
+    $this->assertDatabaseMissing('events', ['id' => $event->id]);
+});
